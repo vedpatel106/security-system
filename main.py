c1ed7fe6d45c632d421707fc3784952c08305662
Vedaant PATEL <PAT0053@wyndhamcentralsc.vic.edu.au>
2023-08-11 12:23:18 +1000

    Email validation added, bugs fixed, security system now fully functional when turned on, live view also fully functional

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from tkinter import *
 from tkinter import ttk
 import tkinter.messagebox as messagebox
 import cv2
+import bcrypt
 import time
 import datetime
-import bcrypt
+from PIL import Image, ImageTk
 
 # Define the COLOUR scheme for the dark theme
 BG_COLOUR = "#424242"
@@ -15,15 +16,15 @@ ENTRY_FG_COLOUR = "#424242"
 BUTTON_BG_COLOUR = "#424242"
 BUTTON_FG_COLOUR = "#23967F"
 MAIN_FONT = "Impact"
-BUTTON_RADIUS = 20  # Radius for rounded edges of buttons
-ENTRY_RADIUS = 20  # Radius for rounded edges of entry fields
+BUTTON_RADIUS = 20
+ENTRY_RADIUS = 20
 
 # ------------------- Security System -------------------
 
 class SecuritySystem:
     def __init__(self, main_menu_window):
         self.main_menu_window = main_menu_window
-        self.security_system_active = False  # Initially, the security system is turned off
+        self.security_system_active = False
         self.cap = cv2.VideoCapture(0)
         self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
         self.body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
@@ -62,7 +63,7 @@ class SecuritySystem:
                         self.detection = False
                         self.timer_started = False
                         self.out.release()
-                        print('Stopped Recording!')
+                        print('Stop Recording!')
                 else:
                     self.timer_started = True
                     self.detection_stopped_time = time.time()
@@ -87,12 +88,11 @@ class LoginWindow:
     def __init__(self, root):
         self.root = root
         self.root.title("Login")
-        self.root.geometry("400x250")  # Set window size
-        self.root.config(bg=BG_COLOUR)  # Set background colour
+        self.root.geometry("400x250")
+        self.root.config(bg=BG_COLOUR)
 
         style = ttk.Style()
-        style.configure("TButton", background=BUTTON_BG_COLOUR, foreground=BUTTON_FG_COLOUR, font=(MAIN_FONT, 10),
-                        relief="flat", borderwidth=0)
+        style.configure("TButton", background=BUTTON_BG_COLOUR, foreground=BUTTON_FG_COLOUR, font=(MAIN_FONT, 10), relief="flat", borderwidth=0)
         style.map("TButton", background=[("active", "#555555")], foreground=[("active", BUTTON_BG_COLOUR)])
 
         self.login_label = Label(root, text="LOGIN", font=(MAIN_FONT, 20), bg=BG_COLOUR, fg=TITLE_COLOUR)
@@ -102,16 +102,16 @@ class LoginWindow:
         self.username_label.pack()
 
         self.username_entry = ttk.Entry(root, style="TEntry", width=20, font=(MAIN_FONT, 10), foreground=ENTRY_FG_COLOUR, background=ENTRY_BG_COLOUR)
-        self.username_entry.pack(pady=5)  # Add vertical padding
+        self.username_entry.pack(pady=5)
 
         self.password_label = Label(root, text="Password:", font=(MAIN_FONT, 10), bg=BG_COLOUR, fg=FG_COLOUR)
         self.password_label.pack()
 
         self.password_entry = ttk.Entry(root, style="TEntry", show="*", width=20, font=(MAIN_FONT, 10), foreground=ENTRY_FG_COLOUR, background=ENTRY_BG_COLOUR)
-        self.password_entry.pack(pady=5)  # Add vertical padding
+        self.password_entry.pack(pady=5)
 
         self.login_button = ttk.Button(root, text="Log In", command=self.login, style="TButton", width=15, cursor="hand2")
-        self.login_button.pack(pady=10)  # Add vertical padding
+        self.login_button.pack(pady=10)
 
         self.register_button = ttk.Button(root, text="Register", command=self.open_registration, style="TButton", width=15, cursor="hand2")
         self.register_button.pack()
@@ -125,7 +125,7 @@ class LoginWindow:
                 email, saved_username, saved_password_hash = line.strip().split(",")
                 if username == saved_username and bcrypt.checkpw(password.encode(), saved_password_hash.encode()):
                     messagebox.showinfo("Login Successful", f"Welcome, {username}!")
-                    self.root.withdraw()  # Hide the login window
+                    self.root.withdraw()
                     main_menu_window = Toplevel(self.root)
                     MainMenuWindow(main_menu_window, self)
                     return
@@ -133,7 +133,7 @@ class LoginWindow:
         messagebox.showerror("Login Failed", "Invalid username or password")
 
     def open_registration(self):
-        self.root.withdraw()  # Hide the login window
+        self.root.withdraw()
         registration_window = Toplevel(self.root)
         RegistrationWindow(registration_window, self.root)
         registration_window.protocol("WM_DELETE_WINDOW", lambda: self.back(registration_window))
@@ -148,9 +148,9 @@ class RegistrationWindow:
     def __init__(self, root, login_window):
         self.root = root
         self.login_window = login_window
-        self.root.geometry("350x425")  # Set window size
+        self.root.geometry("350x425")
         self.root.title("Registration")
-        self.root.config(bg=BG_COLOUR)  # Set background colour
+        self.root.config(bg=BG_COLOUR)
 
         style = ttk.Style()
         style.configure("TEntry", fieldbackground=ENTRY_BG_COLOUR, foreground=ENTRY_FG_COLOUR, bordercolour=BUTTON_BG_COLOUR, borderwidth=0, focuscolour=BUTTON_BG_COLOUR)
@@ -163,37 +163,37 @@ class RegistrationWindow:
         self.email_label.pack()
 
         self.email_entry = ttk.Entry(root, style="TEntry", width=20, font=(MAIN_FONT, 10), foreground=ENTRY_FG_COLOUR, background=ENTRY_BG_COLOUR)
-        self.email_entry.pack(pady=5)  # Add vertical padding
+        self.email_entry.pack(pady=5)
 
         self.username_label = Label(root, text="Username:", font=(MAIN_FONT, 10), bg=BG_COLOUR, fg=FG_COLOUR)
         self.username_label.pack()
 
         self.username_entry = ttk.Entry(root, style="TEntry", width=20, font=(MAIN_FONT, 10), foreground=ENTRY_FG_COLOUR, background=ENTRY_BG_COLOUR)
-        self.username_entry.pack(pady=5)  # Add vertical padding
+        self.username_entry.pack(pady=5)
 
         self.password_label = Label(root, text="Password:", font=(MAIN_FONT, 10), bg=BG_COLOUR, fg=FG_COLOUR)
         self.password_label.pack()
 
         self.password_entry = ttk.Entry(root, style="TEntry", show="*", width=20, font=(MAIN_FONT, 10), foreground=ENTRY_FG_COLOUR, background=ENTRY_BG_COLOUR)
-        self.password_entry.pack(pady=5)  # Add vertical padding
+        self.password_entry.pack(pady=5)
 
         self.confirm_password_label = Label(root, text="Confirm Password:", font=(MAIN_FONT, 10), bg=BG_COLOUR, fg=FG_COLOUR)
         self.confirm_password_label.pack()
 
         self.confirm_password_entry = ttk.Entry(root, style="TEntry", show="*", width=20, font=(MAIN_FONT, 10), foreground=ENTRY_FG_COLOUR, background=ENTRY_BG_COLOUR)
-        self.confirm_password_entry.pack(pady=5)  # Add vertical padding
+        self.confirm_password_entry.pack(pady=5)
 
-        self.blank_line = Label(root, text="", bg=BG_COLOUR, fg=FG_COLOUR) # BLANK LINE
-        self.blank_line.pack() # BLANK LINE
+        self.blank_line = Label(root, text="", bg=BG_COLOUR, fg=FG_COLOUR)
+        self.blank_line.pack()
 
         self.register_button = ttk.Button(root, text="Register", command=self.register, style="TButton", width=15, cursor="hand2")
-        self.register_button.pack(pady=5)  # Add vertical padding
+        self.register_button.pack(pady=5)
 
         self.delete_button = ttk.Button(root, text="Delete User", command=self.delete_user_confirm, style="TButton", width=15, cursor="hand2")
-        self.delete_button.pack(pady=5) # Add vertical padding
+        self.delete_button.pack(pady=5)
 
         self.back_button = ttk.Button(root, text="Back", command=self.back, style="TButton", width=15, cursor="hand2")
-        self.back_button.pack(pady=5) # Add vertical padding
+        self.back_button.pack(pady=5)
 
     def register(self):
         email = self.email_entry.get()
@@ -205,6 +205,8 @@ class RegistrationWindow:
             messagebox.showerror("Error", "Please fill in all fields")
         elif password != confirm_password:
             messagebox.showerror("Error", "Passwords do not match")
+        elif not self.is_valid_email(email):
+            messagebox.showerror("Error", "Invalid email address")
         elif not self.is_strong_password(password):
             messagebox.showerror("Error", "Password must be at least 8 characters long and contain uppercase, lowercase, digit, and special character.")
         else:
@@ -246,6 +248,10 @@ class RegistrationWindow:
         self.password_entry.delete(0, END)
         self.confirm_password_entry.delete(0, END)
 
+    def is_valid_email(self, email):
+        import re
+        return re.match(r"[^@]+@[^@]+\.[^@]+", email)
+
     def is_strong_password(self, password):
         return any(char.isupper() for char in password) and \
                any(char.islower() for char in password) and \
@@ -265,37 +271,47 @@ class MainMenuWindow:
         self.title_label = Label(root, text="Main Menu", font=(MAIN_FONT, 20), bg=BG_COLOUR, fg=TITLE_COLOUR)
         self.title_label.pack(pady=10)
 
-        self.live_view_button = ttk.Button(root, text="Live View", command=self.open_live_view, style="TButton", width=15,
-                                           cursor="hand2")
+        self.live_view_button = ttk.Button(root, text="Live View", command=self.open_live_view, style="TButton", width=15, cursor="hand2")
         self.live_view_button.pack(pady=5)
 
-        self.activity_log_button = ttk.Button(root, text="View Activity Log", command=self.open_activity_log,
-                                              style="TButton", width=20, cursor="hand2")
+        self.activity_log_button = ttk.Button(root, text="View Activity Log", command=self.open_activity_log, style="TButton", width=20, cursor="hand2")
         self.activity_log_button.pack(pady=5)
 
-        self.print_report_button = ttk.Button(root, text="Print Report", command=self.open_print_report,
-                                              style="TButton", width=15, cursor="hand2")
+        self.print_report_button = ttk.Button(root, text="Print Report", command=self.open_print_report, style="TButton", width=15, cursor="hand2")
         self.print_report_button.pack(pady=5)
 
-        self.exit_button = ttk.Button(root, text="Exit", command=self.root.quit, style="TButton", width=15, cursor="hand2")
+        self.exit_button = ttk.Button(root, text="Exit", command=self.exit_program, style="TButton", width=15, cursor="hand2")
         self.exit_button.pack(pady=5)
 
         self.security_button_text = StringVar()
         self.security_button_text.set("Turn On Security System")
-        self.security_button = ttk.Button(root, textvariable=self.security_button_text, command=self.toggle_security_system,
-                                         style="TButton", width=20, cursor="hand2")
+        self.security_button = ttk.Button(root, textvariable=self.security_button_text, command=self.toggle_security_system, style="TButton", width=20, cursor="hand2")
         self.security_button.pack(pady=5)
 
-        self.security_system = SecuritySystem(self)  # Pass the MainMenuWindow instance to SecuritySystem
+        self.video_feed_label = Label(root, bg=BG_COLOUR)
+        self.video_feed_label.pack()
+
+        self.security_system = SecuritySystem(self)
         self.security_system.check_security()
+
+    def update_video_feed(self, label):
+        _, frame = self.security_system.cap.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image = Image.fromarray(frame)
+        photo = ImageTk.PhotoImage(image=image)
+        label.imgtk = photo
+        label.configure(image=photo)
+        label.after(10, lambda: self.update_video_feed(label))
 
     def open_live_view(self):
         live_view_window = Toplevel(self.root)
         live_view_window.title("Live View")
-        live_view_window.geometry("400x200")
+        live_view_window.geometry("400x300")
         live_view_window.config(bg=BG_COLOUR)
 
-        # Add the live feed of the camera here
+        video_feed_label = Label(live_view_window, bg=BG_COLOUR)
+        video_feed_label.pack()
+        self.update_video_feed(video_feed_label)
 
     def open_activity_log(self):
         activity_log_window = Toplevel(self.root)
@@ -321,15 +337,23 @@ class MainMenuWindow:
             self.security_system.start_security_system()
             self.security_button_text.set("Turn Off Security System")
 
-    def exit_program(root, login_window):
-        if login_window.security_system.security_system_active:
-            login_window.security_system.release_camera()
-        root.destroy()
+    def exit_program(self):
+        self.security_system.release_camera()  # Release camera resources
+        self.root.withdraw()  # Hide the current window
+        self.login_window.root.deiconify()  # Show the login window
 
 if __name__ == "__main__":
     root = Tk()
-    root.config(bg=BG_COLOUR)  # Set background colour
+    root.config(bg=BG_COLOUR)
     login_window = LoginWindow(root)
 
-    root.protocol("WM_DELETE_WINDOW", lambda: exit())
+    def on_closing():
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            if hasattr(login_window, 'main_menu_window'):
+                main_menu_window = login_window.main_menu_window
+                if main_menu_window.security_system.security_system_active:
+                    main_menu_window.security_system.release_camera()
+                main_menu_window.exit_program()  # Call the exit_program method
+
+    root.protocol("WM_DELETE_WINDOW", on_closing)
     root.mainloop()
